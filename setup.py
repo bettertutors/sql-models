1569c29d54792431d2d4eabffb401884c2dd5d4a
Samuel Marks <samuelmarks@gmail.com>
2015-02-20 00:18:57 +1100

    Removing `package_dir`

--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ if __name__ == '__main__':
     setup(
         name=package_name,
         author='Samuel Marks',
-        version='0.1.3',
+        version='0.1.4',
         test_suite=package_name + '.tests',
         packages=find_packages(exclude='*test'),
-        package_dir={package_name: package_name},
         package_data={package_name: ['logging.conf']},
         install_requires=[
             'peewee==2.4.7',
