a512406d6146ba2cbe1454e708d95cb6bd65cefe
Samuel Marks <samuelmarks@gmail.com>
2015-03-08 23:00:26 +1100

    API is making more sense (hopefully!)

--- bettertutors_sql_models/utils/__init__.py
+++ bettertutors_sql_models/utils/__init__.py
@@ -1,5 +1,5 @@
-from bettertutors_sql_models.models.Signup import Signup
+from bettertutors_sql_models.base import db
 
 row_to_dict = lambda row: {key: getattr(row, key) for key in row._meta.fields.keys()}
 
-create_tables = lambda db: db.create_tables([Signup], safe=True)
+create_tables = lambda models: db.create_tables(models, safe=True)
